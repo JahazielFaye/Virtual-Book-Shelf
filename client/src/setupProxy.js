ec49bc7e6b0164f2c2e1c5b018ec75e5219ece84
zxielle <@JahazielFaye>
2023-03-27 04:01:50 -0400

    deploy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
      
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api/**', { target: 'https://my-api.herokuapp.com' }));
+    app.use(createProxyMiddleware('/api/**', { target: 'http://localhost:5000' }));
+    app.use(createProxyMiddleware('/otherApi/**', { target: 'http://localhost:5000' }));
 };
